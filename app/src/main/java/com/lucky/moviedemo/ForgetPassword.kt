66def1e1beca0ec7db488c358f8af3839db1b714
ATK <aungthukhant2001@gmail.com>
2025-11-11 20:32:52 +0700

    login

--- app/src/main/java/com/lucky/moviedemo/ForgetPassword.kt
+++ app/src/main/java/com/lucky/moviedemo/ForgetPassword.kt
@@ -1,10 +1,15 @@
 package com.lucky.moviedemo
 
+import android.content.Intent
+import android.net.NetworkCapabilities
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 import com.lucky.moviedemo.databinding.ActivityForgetPasswordBinding
 
 class ForgetPassword : AppCompatActivity() {
@@ -12,7 +17,42 @@ class ForgetPassword : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityForgetPasswordBinding.inflate(layoutInflater)
+
+        binding.btnNext.setOnClickListener {
+            var email = binding.etEmail.editText?.text.toString()
+            if(TextUtils.isEmpty(email)){
+                binding.etEmail.error = "Email is required"
+                binding.etEmail.requestFocus()
+            }else{
+                if(checkInternetConnection()){
+                    sendPasswordResetLink(email)
+                }else{
+                    Toast.makeText(this,"No Internet Connection",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
         setContentView(binding.root)
 
     }
+
+    private fun sendPasswordResetLink(email: String) {
+        FirebaseAuth.getInstance().sendPasswordResetEmail(email).addOnCompleteListener { task ->
+            if(task.isSuccessful){
+                Toast.makeText(this,"Password Reset Link Sent to your Email",Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, Login::class.java))
+                finish()
+            }else{
+                Toast.makeText(this,"Failed to send Password Reset Link",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun checkInternetConnection() : Boolean{
+        val connectivityManager = getSystemService(CONNECTIVITY_SERVICE) as android.net.ConnectivityManager
+        val network = connectivityManager.activeNetwork ?: return false
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
+        return networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+    }
+
 }
--- app/src/main/java/com/lucky/moviedemo/Login.kt
+++ app/src/main/java/com/lucky/moviedemo/Login.kt
@@ -1,10 +1,15 @@
 package com.lucky.moviedemo
 
+import android.content.Intent
+import android.net.NetworkCapabilities
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 import com.lucky.moviedemo.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
@@ -13,12 +18,70 @@ class Login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
 
+        val sharedPref = getSharedPreferences("SaveInfo", MODE_PRIVATE)
+        val isChecked = sharedPref.getBoolean("isChecked", false)
+        if(isChecked){
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
 
         binding.btnSignUp.setOnClickListener {
-            startActivity(android.content.Intent(this, SignUp::class.java))
+            startActivity(Intent(this, SignUp::class.java))
+        }
+
+        binding.btnLogin.setOnClickListener {
+            var email = binding.timail.editText?.text.toString()
+            var password = binding.tiPassword.editText?.text.toString()
+            if(TextUtils.isEmpty(email)){
+                binding.timail.error = "Email is required"
+                binding.timail.requestFocus()
+            }else if(TextUtils.isEmpty(password)){
+                binding.tiPassword.error = "Password is required"
+                binding.tiPassword.requestFocus()
+            }else{
+                if(checkInternetConnection()){
+                    loginAccount(email,password)
+                }else{
+                    Toast.makeText(this,"No Internet Connection",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        binding.txtForgotPassword.setOnClickListener {
+            startActivity(android.content.Intent(this, ForgetPassword::class.java))
         }
 
         setContentView(binding.root)
 
     }
+    private fun checkInternetConnection() : Boolean{
+        val connectivityManager = getSystemService(CONNECTIVITY_SERVICE) as android.net.ConnectivityManager
+        val network = connectivityManager.activeNetwork ?: return false
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
+        return networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+    }
+    private fun loginAccount(email: String, password: String) {
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+            if(task.isSuccessful){
+                if(binding.checkBox.isChecked){
+                    val sharedPref = getSharedPreferences("SaveInfo", MODE_PRIVATE)
+                    val editor = sharedPref.edit()
+                    editor.putString("email", email)
+                    editor.putBoolean("isChecked", true)
+                    editor.apply()
+                }else{
+                    val sharedPref = getSharedPreferences("SaveInfo", MODE_PRIVATE)
+                    val editor = sharedPref.edit()
+                    editor.putBoolean("isChecked", false)
+                    editor.apply()
+                }
+                Toast.makeText(this,"Login Successfully",Toast.LENGTH_SHORT).show()
+                startActivity(android.content.Intent(this, MainActivity::class.java))
+                finish()
+            }else{
+                Toast.makeText(this,"Login Failed!",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 }
